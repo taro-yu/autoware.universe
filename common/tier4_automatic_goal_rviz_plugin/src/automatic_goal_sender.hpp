56146e9d9e2389a492b86f4b9ea55177c6c46614
taro-yu <milktea1621@gmai.com>
2024-07-12 14:04:09 +0900

    feat: add goal name display
    Signed-off-by: taro-yu <milktea1621@gmai.com>

--- common/tier4_automatic_goal_rviz_plugin/src/automatic_goal_sender.hpp
+++ common/tier4_automatic_goal_rviz_plugin/src/automatic_goal_sender.hpp
@@ -34,7 +34,7 @@
 #include <memory>
 #include <string>
 #include <utility>
-#include <vector>
+extern bool G_HAS_GOAL_NAME;
 
 namespace automatic_goal
 {
@@ -156,6 +156,7 @@ protected:
   unsigned current_goal_{0};
   State state_{State::INITIALIZING};
   std::vector<PoseStamped::ConstSharedPtr> goals_list_{};
+  std::vector<std::string> goals_name_list_{};
   std::map<unsigned, std::pair<std::string, unsigned>> goals_achieved_{};
   std::string goals_achieved_file_path_{};
 
